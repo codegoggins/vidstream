dca3c1f782b8f521b868605697287c8accd92489
Nilay Singh <snilay011@gmail.com>
2023-03-11 01:25:35 +0530

    Footer Complete

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color:#1c1d1f;
+  height: 5rem;
+  width:full;
+  padding: 1rem 3rem;
+  margin-top: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.div`
+font-weight: bold;
+display: flex;
+font-size: 1.5rem;
+cursor: pointer;
+
+span{
+    color: #e473ff;
+}
+p{
+    color: white;
+}
+
+`;
+
+const Text = styled.p`
+ color: white;
+`
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container>
+        <Logo>
+        <span>V</span>
+        <p>stream</p>
+        </Logo>
+        <Text>© Developed by <a href='https://github.com/codegoggins'><em>codegoggins.</em></a></Text>
+    </Container>
   )
 }
 
