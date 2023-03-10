daa372f2b31436cc6ce90def1fac9925e15e5579
Nilay Singh <snilay011@gmail.com>
2023-03-11 00:35:21 +0530

    Single Comment Component Completed

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+display: flex;
+align-items:flex-start;
+gap: 0.8rem;
+justify-content: center;
+font-size: 0.9rem;
+
+.user-img{
+    height: 2.5rem;
+    width: 2.5rem;
+    object-fit: cover;
+    border-radius: 0.5rem;
+}
+
+.user-comment{
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+
+    .coment-content{
+        color: whitesmoke;
+    }
+}
+
+
+`;
 
 const Comment = () => {
   return (
-    <div>Comment</div>
+    <Container>
+         <img className='user-img' src='https://images.pexels.com/photos/13010778/pexels-photo-13010778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
+         <div className='user-comment'>
+            <p className='user-name'>Nilay Singh</p>
+            <p className='coment-content'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id velit quas eveniet. Voluptate excepturi minus perferendis harum vero libero accusantium, rerum nesciunt, consequuntur quidem dolores? Voluptatem ullam maxime ipsum minima?
+            </p>
+         </div>
+    </Container>
   )
 }
 
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import Comment from './Comment';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 
 const Container = styled.div`
 display: flex;
 flex-direction: column;
-gap: 1rem;
-padding: 0rem 3rem;
+gap: 2rem;
+padding: 0rem 3rem 5rem 3rem;
 
 input{
+    width: 80%;
     font-size: 1rem;
     border: none;
+    border-bottom: 0.08rem solid white;
     padding: 0.8rem 8rem 0.8rem 0.5rem;
     border-radius: 0.3rem;
-    background-color: #1c1d1f;
+    background:transparent;
     color: grey;
     &::placeholder{
         color: grey;
@@ -23,9 +26,17 @@ input{
     }
 }
 
+.post-comment{
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    cursor: pointer;
+}
+
 .comments{
     display: flex;
     flex-direction: column;
+    gap: 2rem;
 }
 
 `;
@@ -34,8 +45,17 @@ const CommentSection = () => {
   return (
     <Container>
         <p className='title'>Comments</p>
-        <input type="text" placeholder='Enter a comment'/>
+        <div className='post-comment'>
+            <input type="text" placeholder='Enter a comment'/>
+            <p><SendOutlinedIcon/></p>
+        </div>
         <div className='comments'>
+            <Comment/>
+            <Comment/>
+            <Comment/>
+            <Comment/>
+            <Comment/>
+            <Comment/>
             <Comment/>
         </div>
     </Container>
